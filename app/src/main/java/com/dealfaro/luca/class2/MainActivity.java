78ecf822fcfb1336f9a37affc2ad21f9ead2d57e
Tyler <tylertagawa@gmail.com>
2016-01-18 18:48:00 -0800

    made x transparent. Game now functions properly. Just need to make it prettier.

--- app/src/main/java/com/dealfaro/luca/class2/MainActivity.java
+++ app/src/main/java/com/dealfaro/luca/class2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dealfaro.luca.class2;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +21,24 @@ public class MainActivity extends AppCompatActivity {
                 ticked[i][j]=-1;
             }
         }
+        ImageButton b = (ImageButton) findViewById(R.id.imageButton00);
+        b.setBackgroundResource(android.R.drawable.btn_default);
+        b = (ImageButton) findViewById(R.id.imageButton01);
+        b.setBackgroundResource(android.R.drawable.btn_default);
+        b = (ImageButton) findViewById(R.id.imageButton02);
+        b.setBackgroundResource(android.R.drawable.btn_default);
+        b = (ImageButton) findViewById(R.id.imageButton10);
+        b.setBackgroundResource(android.R.drawable.btn_default);
+        b = (ImageButton) findViewById(R.id.imageButton11);
+        b.setBackgroundResource(android.R.drawable.btn_default);
+        b = (ImageButton) findViewById(R.id.imageButton12);
+        b.setBackgroundResource(android.R.drawable.btn_default);
+        b = (ImageButton) findViewById(R.id.imageButton20);
+        b.setBackgroundResource(android.R.drawable.btn_default);
+        b = (ImageButton) findViewById(R.id.imageButton21);
+        b.setBackgroundResource(android.R.drawable.btn_default);
+        b = (ImageButton) findViewById(R.id.imageButton22);
+        b.setBackgroundResource(android.R.drawable.btn_default);
     }
 
     public void putCross(View v) {
@@ -56,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
                     c.setText("O won!");
                 else
                     c. setText("X won!");
+                highlight('d',0);
                 gameWin(v);
                 turn=-1;
             }
@@ -64,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
                     c.setText("O won!");
                 else
                     c. setText("X won!");
+                highlight('d',1);
                 gameWin(v);
                 turn=-1;
             }
@@ -73,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
                         c.setText("O won!");
                     else
                         c. setText("X won!");
+                    highlight('r',j);
                     gameWin(v);
                     turn=-1;
 
@@ -82,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
                         c.setText("O won!");
                     else
                         c. setText("X won!");
+                    highlight('c',j);
                     gameWin(v);
                     turn=-1;
                 }
@@ -93,34 +116,124 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void highlight(char type,int line){
+        ImageButton b = (ImageButton) findViewById(R.id.imageButton00);
+        if(type == 'r'){
+            switch (line) {
+                case 0:
+                    b = (ImageButton) findViewById(R.id.imageButton00);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton01);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton02);
+                    b.setBackgroundColor(Color.GREEN);
+                    break;
+                case 1:
+                    b = (ImageButton) findViewById(R.id.imageButton10);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton11);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton12);
+                    b.setBackgroundColor(Color.GREEN);
+                    break;
+                case 2:
+                    b = (ImageButton) findViewById(R.id.imageButton20);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton21);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton22);
+                    b.setBackgroundColor(Color.GREEN);
+                    break;
+            }
+        }
+        if(type == 'c'){
+            switch(line){
+                case 0:
+                    b = (ImageButton) findViewById(R.id.imageButton00);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton10);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton20);
+                    b.setBackgroundColor(Color.GREEN);
+                    break;
+                case 1:
+                    b = (ImageButton) findViewById(R.id.imageButton01);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton11);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton21);
+                    b.setBackgroundColor(Color.GREEN);
+                    break;
+                case 2:
+                    b = (ImageButton) findViewById(R.id.imageButton02);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton12);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton22);
+                    b.setBackgroundColor(Color.GREEN);
+                    break;
+            }
+        }
+        if(type == 'd'){
+            switch (line){
+                case 0:
+                    b = (ImageButton) findViewById(R.id.imageButton00);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton11);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton22);
+                    b.setBackgroundColor(Color.GREEN);
+                    break;
+                case 1:
+                    b = (ImageButton) findViewById(R.id.imageButton02);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton11);
+                    b.setBackgroundColor(Color.GREEN);
+                    b = (ImageButton) findViewById(R.id.imageButton20);
+                    b.setBackgroundColor(Color.GREEN);
+                    break;
+            }
+        }
+
+    }
+
     public void newGame(View v){
         ImageButton b = (ImageButton) findViewById(R.id.imageButton00);
         b.setImageResource(R.drawable.blank);
         b.setEnabled(true);
+        b.setBackgroundResource(android.R.drawable.btn_default);
         b = (ImageButton) findViewById(R.id.imageButton01);
         b.setImageResource(R.drawable.blank);
         b.setEnabled(true);
+        b.setBackgroundResource(android.R.drawable.btn_default);
         b = (ImageButton) findViewById(R.id.imageButton02);
         b.setImageResource(R.drawable.blank);
         b.setEnabled(true);
+        b.setBackgroundResource(android.R.drawable.btn_default);
         b = (ImageButton) findViewById(R.id.imageButton10);
         b.setImageResource(R.drawable.blank);
         b.setEnabled(true);
+        b.setBackgroundResource(android.R.drawable.btn_default);
         b = (ImageButton) findViewById(R.id.imageButton11);
         b.setImageResource(R.drawable.blank);
         b.setEnabled(true);
+        b.setBackgroundResource(android.R.drawable.btn_default);
         b = (ImageButton) findViewById(R.id.imageButton12);
         b.setImageResource(R.drawable.blank);
         b.setEnabled(true);
+        b.setBackgroundResource(android.R.drawable.btn_default);
         b = (ImageButton) findViewById(R.id.imageButton20);
         b.setImageResource(R.drawable.blank);
         b.setEnabled(true);
+        b.setBackgroundResource(android.R.drawable.btn_default);
         b = (ImageButton) findViewById(R.id.imageButton21);
         b.setImageResource(R.drawable.blank);
         b.setEnabled(true);
+        b.setBackgroundResource(android.R.drawable.btn_default);
         b = (ImageButton) findViewById(R.id.imageButton22);
         b.setImageResource(R.drawable.blank);
         b.setEnabled(true);
+        b.setBackgroundResource(android.R.drawable.btn_default);
         turn = 0;
         for(int i = 0; i<3;i++){
             for(int j = 0; j<3; j++){
